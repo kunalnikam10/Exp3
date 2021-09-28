d1e1ab48f7b1aaa2c22a16a4c3fc32822d02a6c9
kunalnikam10 <kunalnikam1550@gmail.com>
2021-09-28 21:22:15 +0530

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,9 @@
+class Employee{
+    public static void main (String[] args){
+        System.out.println("\nName \t Year of joining\t Salary\t    Address  ");
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Jonny\t  2001\t            50000\t      Paris ");
+        System.out.println("Tanmay\t  2002\t            75000\t     Behlapur ");
+        System.out.println("Namam \t  2003\t            20000\t     Andheri ");
+    }
+}
